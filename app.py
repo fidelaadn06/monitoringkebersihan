4fcb523fb8c2cfbf88fbb02c2b00c4d29de4a7d9
fidelaadn06 <fidelaadn06@gmail.com>
2026-02-04 15:47:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -378,7 +378,7 @@ PEKERJAAN = {
 # =====================================================
 # LOAD DATA
 # =====================================================
-data = pd.read_csv("Form4.csv")
+data = pd.read_csv("form5.csv")
 data["Tanggal"] = pd.to_datetime(
     data["Tanggal Pelaksanaan  "],
     dayfirst=True,
